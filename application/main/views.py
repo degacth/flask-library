420a7a37b72ea18c613a75deb27904967567a3c0
Alexander Degtarov <degacth@yandex.ru>
2017-04-08 13:04:28 +0300

    make angular run

--- application/main/views.py
+++ application/main/views.py
@@ -4,7 +4,7 @@ from flask import Blueprint, render_template
 from config import Config
 
 SRC_DIR = os.path.join(Config.BASE_DIR, 'src')
-main_bp = Blueprint('main', __name__, template_folder=SRC_DIR)
+main_bp = Blueprint('main', __name__, template_folder=SRC_DIR, static_folder=SRC_DIR)
 
 
 @main_bp.route('/')
