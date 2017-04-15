01d3ab4d2863ec5f9297472e070214daf34f72da
Alexander Degtarov <degacth@yandex.ru>
2017-04-15 20:15:10 +0300

    Add author and book routes

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,9 +2,10 @@ import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {HomeComponent} from "./home.component";
 import {ModelModule} from "../model/model.module";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
-    imports: [ModelModule, BrowserModule],
+    imports: [ModelModule, BrowserModule, RouterModule],
     declarations: [HomeComponent],
     exports: [HomeComponent],
 })
