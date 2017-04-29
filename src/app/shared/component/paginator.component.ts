233b8a747435a6781ee26407b66b7d420d2c548d
Alexander Degtarov <degacth@yandex.ru>
2017-04-29 18:04:33 +0300

    show paginator pages

--- src/app/shared/component/paginator.component.ts
+++ src/app/shared/component/paginator.component.ts
@@ -11,7 +11,33 @@ export class Paginator {
     @Input('resource')
     resource: IPaginator;
 
-    getPages: () => number[] = () => {
-        return [1, 2, 3];
+    getPages: () => Page[] = () => {
+        let activePage: number = this.resource.getPage();
+        let totalPages: number = this.resource.getTotalPages();
+        let minPage: number = 1;
+        let pages: number[] = _.uniq([
+            Math.max(minPage, activePage - 1),
+            activePage,
+            Math.min(totalPages, activePage + 1),
+        ]);
+
+        // set bounds and separators
+        let pageSeparator: number = 0;
+        if (_.head(pages) != minPage) pages = _.concat([minPage, pageSeparator], pages);
+        if (_.last(pages) != totalPages ) pages = _.concat(pages, [pageSeparator, totalPages]);
+
+        return _.concat(
+            _.map(pages, page => new Page(page, page === activePage)),
+        )
+    }
+}
+
+class Page {
+    isLink: boolean;
+    label: string;
+
+    constructor(public position: number, public isActive: boolean) {
+        this.isLink = position && !isActive;
+        this.label = (position || '...') as string;
     }
 }
